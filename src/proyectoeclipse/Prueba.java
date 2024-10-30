03ff99009c000c522be3700cd1614c8095ae5643
Susana Salas <ssalmar2509@g.educaand.es>
2024-10-30 09:17:54 +0100

    version inicial

---
+++ src/proyectoeclipse/Prueba.java
@@ -0,0 +1,13 @@
+package proyectoeclipse;
+
+public class Prueba {
+
+	public static void main (String[] args) {
+		
+		System.out.println("Hola Mundo!!!");
+		
+		System.out.println("Adios!!!");
+		
+	}
+	
+}
